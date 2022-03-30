aadb62c02a61e90b5aa2fbede9fd7646478addf2
StackSpot Admin <admin@stackspot.com>
2022-03-30 03:35:36 +0000

    Update public repository

--- main.py
+++ main.py
@@ -8,28 +8,32 @@ class Plugin(Template):
     def post_hook(self, metadata: Metadata):
         project_name = metadata.global_inputs['project_name']
 
-        os.chdir(f'{metadata.target_path}/src/{project_name}.Domain/')
-
         if 'Prometheus' in metadata.inputs['type']:
+            os.chdir(f'{metadata.target_path}/src/{project_name}.Application/')
             subprocess.run(['dotnet', 'add', 'package', 'StackSpot.Metrics'])
-            using = "using StackSpot.Metrics;\nusing Prometheus;"
+            
+            os.chdir(f'{metadata.target_path}/src/{project_name}.Infrastructure/')
+            subprocess.run(['dotnet', 'add', 'package', 'StackSpot.Metrics'])            
+            
+            using = "using StackSpot.Metrics;using Prometheus;"
             service = "services.ConfigureMetrics();"
             app = "app.UseMetricServer();\napp.UseHttpMetrics();"
         
         print('Setting Configuration...')
 
-        os.chdir(f'{metadata.target_path}/src/{project_name}.Api/')
+        os.chdir(f'{metadata.target_path}/src/{project_name}.Application/Common/StackSpot/')
 
-        configuration_file = open(file='ConfigurationStackSpot.cs', mode='r')
+        configuration_file = open(file='DependencyInjection.cs', mode='r')
         content = configuration_file.readlines()
         index = [x for x in range(len(content)) if 'return services' in content[x].lower()]
         index_app = [x for x in range(len(content)) if 'return app' in content[x].lower()]
+        index_using = [x for x in range(len(content)) if 'using' in content[x].lower()]
 
-        content[0] = using+content[0]
         content[index[0]] = f"{service}\n{content[index[0]]}"
         content[index_app[0]] = f"{app}\n{content[index_app[0]]}"        
-        
-        configuration_file = open(file='ConfigurationStackSpot.cs', mode='w')                     
+        content[index_using[0]] = f"{using}\n{content[index_using[0]]}"
+
+        configuration_file = open(file='DependencyInjection.cs', mode='w')                     
         configuration_file.writelines(content)
         configuration_file.close()
 
@@ -37,7 +41,7 @@ class Plugin(Template):
 
         print('Apply dotnet format...')
         os.chdir(f'{metadata.target_path}/')
-        subprocess.run(['dotnet', 'dotnet-format', './src'])   
+        subprocess.run(['dotnet', 'dotnet-format', f'src/{project_name}.Application/{project_name}.Application.csproj', '--include-generated'])    
         print('Apply dotnet format done...')
 
 if __name__ == '__main__':
